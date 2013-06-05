a6c2161a61071676fa07fce427bebcd910aee881
Joseph Adams <whitegolem@gmail.com>
2013-06-05 17:32:22 +0200

    added first non-concurrent costFunction

--- main.go
+++ main.go
@@ -7,10 +7,48 @@ import (
 )
 
 func main() {
-	input := matrix.FromMatlab("[1;1]")
+	//input := matrix.FromMatlab("[1;1]")
 	thetas := []*matrix.Matrix{matrix.FromMatlab("[-30 20 20]")}
 
-	fmt.Println(hypothesis(thetas, input))
+	//fmt.Println(hypothesis(thetas, input))
+
+	data := [][]*matrix.Matrix{[]*matrix.Matrix{matrix.FromMatlab("[1; 1]"), matrix.FromMatlab("[1]")}, []*matrix.Matrix{matrix.FromMatlab("[0; 1]"), matrix.FromMatlab("[0]")}, []*matrix.Matrix{matrix.FromMatlab("[1; 0]"), matrix.FromMatlab("[0]")}, []*matrix.Matrix{matrix.FromMatlab("[0; 0]"), matrix.FromMatlab("[0]")}}
+
+	fmt.Println(costFunction(data, thetas, 0.04))
+
+}
+
+func costFunction(data [][]*matrix.Matrix, thetas []*matrix.Matrix, lambda float64) float64 {
+	cost := float64(0)
+	var estimation []float64
+	var expected_output []float64
+
+	// Cost
+
+	for _, datum := range data {
+		estimation = hypothesis(thetas, datum[0]).Values()
+		expected_output = datum[1].Values()
+
+		for k, y := range expected_output {
+			cost += y*math.Log(estimation[k]) + (1-y)*math.Log(1-estimation[k])
+		}
+	}
+
+	// Regularization
+	regularizationCost := float64(0)
+
+	for _, theta := range thetas {
+		for i, param := range theta.Values() {
+			if i%theta.Columns() == 0 {
+				continue
+			}
+
+			regularizationCost += param * param
+		}
+	}
+
+	return cost/float64(len(data)) + (lambda/(2*float64(len(data))))*regularizationCost
+
 }
 
 func hypothesis(thetas []*matrix.Matrix, input *matrix.Matrix) *matrix.Matrix {
