15d97719ef3540f469aad4eae7cddd5141e60cd9
Joseph Adams <whitegolem@gmail.com>
2013-06-05 18:19:44 +0200

    tried to implement a deltaTerms func but didn't succeed so far.

--- main.go
+++ main.go
@@ -8,14 +8,24 @@ import (
 
 func main() {
 	//input := matrix.FromMatlab("[1;1]")
-	thetas := []*matrix.Matrix{matrix.FromMatlab("[-30 20 20]")}
+	thetas := []*matrix.Matrix{matrix.FromMatlab("[-30 20 20; 10 20 40; 40 5 20]"), matrix.FromMatlab("[-30 20 20; 50 20 10]")}
 
 	//fmt.Println(hypothesis(thetas, input))
 
-	data := [][]*matrix.Matrix{[]*matrix.Matrix{matrix.FromMatlab("[1; 1]"), matrix.FromMatlab("[1]")}, []*matrix.Matrix{matrix.FromMatlab("[0; 1]"), matrix.FromMatlab("[0]")}, []*matrix.Matrix{matrix.FromMatlab("[1; 0]"), matrix.FromMatlab("[0]")}, []*matrix.Matrix{matrix.FromMatlab("[0; 0]"), matrix.FromMatlab("[0]")}}
+	//data := [][]*matrix.Matrix{[]*matrix.Matrix{matrix.FromMatlab("[1; 1]"), matrix.FromMatlab("[1]")}, []*matrix.Matrix{matrix.FromMatlab("[0; 1]"), matrix.FromMatlab("[0]")}, []*matrix.Matrix{matrix.FromMatlab("[1; 0]"), matrix.FromMatlab("[0]")}, []*matrix.Matrix{matrix.FromMatlab("[0; 0]"), matrix.FromMatlab("[0]")}}
 
-	fmt.Println(costFunction(data, thetas, 0.04))
+	//fmt.Println(costFunction(data, thetas, 0.04))
 
+	fmt.Println(deltaTerms(thetas, matrix.FromMatlab("[0.5; 0.4234235]"), matrix.FromMatlab("[1; 0]")))
+
+}
+
+func deltaTerms(thetas []*matrix.Matrix, estimation, expected_output *matrix.Matrix) []*matrix.Matrix {
+	deltas := make([]*matrix.Matrix, len(thetas))
+
+	deltas[len(deltas)-1], _ = estimation.Sub(expected_output)
+
+	return deltas
 }
 
 func costFunction(data [][]*matrix.Matrix, thetas []*matrix.Matrix, lambda float64) float64 {
@@ -73,4 +83,12 @@ func sigmoidMatrix(index int, value float64) float64 {
 
 func sigmoid(z float64) float64 {
 	return 1 / (1 + math.Pow(math.E, -z))
+}
+
+func sigmoidGradientMatrix(index int, value float64) float64 {
+	return sigmoidGradient(value)
+}
+
+func sigmoidGradient(z float64) float64 {
+	return sigmoid(z) * (1 - sigmoid(z))
 }
